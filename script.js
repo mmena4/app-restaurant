7b459a5e91acc3de86a5b55444a7f0a57e349f3f
Moi <mmena4@hotmail.com>
2021-01-18 14:38:12 +0100

    JavaScript product cards functionality added

---
+++ script.js
@@ -0,0 +1,97 @@
+function showDesayunos() {
+    const desayunos = document.querySelector('#desayunos');
+    desayunos.style.display = 'flex';
+    tapas.style.display = 'none';
+    ensaladas.style.display = 'none';
+    almuerzos.style.display = 'none';
+    cena.style.display = 'none';
+    postres.style.display = 'none';
+    bebidas.style.display = 'none';
+    todos.style.display = 'none';
+};
+
+function showTapas() {
+    const tapas = document.querySelector('#tapas');
+    tapas.style.display = 'flex';
+    desayunos.style.display = 'none';
+    ensaladas.style.display = 'none';
+    almuerzos.style.display = 'none';
+    cena.style.display = 'none';
+    postres.style.display = 'none';
+    bebidas.style.display = 'none';
+    todos.style.display = 'none';
+};
+
+function showEnsaladas() {
+    const ensaladas = document.querySelector('#ensaladas');
+    ensaladas.style.display = 'flex';
+    tapas.style.display = 'none';
+    desayunos.style.display = 'none';
+    almuerzos.style.display = 'none';
+    cena.style.display = 'none';
+    postres.style.display = 'none';
+    bebidas.style.display = 'none';
+    todos.style.display = 'none';
+};
+
+function showAlmuerzos() {
+    const almuerzos = document.querySelector('#almuerzos');
+    almuerzos.style.display = 'flex';
+    tapas.style.display = 'none';
+    desayunos.style.display = 'none';
+    ensaladas.style.display = 'none';
+    cena.style.display = 'none';
+    postres.style.display = 'none';
+    bebidas.style.display = 'none';
+    todos.style.display = 'none';
+};
+
+function showCena() {
+    const cena = document.querySelector('#cena');
+    cena.style.display = 'flex';
+    tapas.style.display = 'none';
+    desayunos.style.display = 'none';
+    ensaladas.style.display = 'none';
+    almuerzos.style.display = 'none';
+    postres.style.display = 'none';
+    bebidas.style.display = 'none';
+    todos.style.display = 'none';
+};
+
+function showPostres() {
+    const postres = document.querySelector('#postres');
+    postres.style.display = 'flex';
+    tapas.style.display = 'none';
+    desayunos.style.display = 'none';
+    almuerzos.style.display = 'none';
+    cena.style.display = 'none';
+    ensaladas.style.display = 'none';
+    bebidas.style.display = 'none';
+    todos.style.display = 'none';
+};
+
+function showBebidas() {
+    const bebidas = document.querySelector('#bebidas');
+    bebidas.style.display = 'flex';
+    tapas.style.display = 'none';
+    desayunos.style.display = 'none';
+    almuerzos.style.display = 'none';
+    cena.style.display = 'none';
+    postres.style.display = 'none';
+    ensaladas.style.display = 'none';
+    todos.style.display = 'none';
+};
+
+function showTodos() {
+    const todos = document.querySelector('#todos');
+    tapas.style.display = 'flex';
+    desayunos.style.display = 'flex';
+    ensaladas.style.display = 'flex';
+    almuerzos.style.display = 'flex';
+    cena.style.display = 'flex';
+    postres.style.display = 'flex';
+    bebidas.style.display = 'flex';   
+};
+
+
+
